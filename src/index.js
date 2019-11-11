c705c2f70d6293fd9d5ebcb71d42d33fa186b957
Sabrina Pomajulca <sabrina.pomajulca@tecsup.edu.pe>
2019-11-11 09:37:12 -0500

    commit3

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ const passport = require('passport');
 const flash = require('connect-flash');
 const MySQLStore = require('express-mysql-session')(session);
 const bodyParser = require('body-parser');
-
 const { database } = require('./keys');
 
 // Intializations
